50eac5c71a122a2b427fd0bfad336b07fd6a81a2
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-10-11 16:48:28 +0300

    thh

--- src/index.js
+++ src/index.js
@@ -8,48 +8,80 @@ const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
 select.addEventListener('change', onSelectClick);
+
 loader.classList.add('hidden');
 error.classList.add('hidden');
 
 let arrayInfo = [];
+let arrayAbout = [];
 
 fetchBreeds()
   .then(resp => {
     resp.map(el => {
+      // console.log(el);
       arrayInfo.push({ text: el.name, value: el.id });
     });
+
     // console.log(arrayInfo);
     new SlimSelect({
       select: '.breed-select',
       data: arrayInfo,
     });
+    return resp.map(el =>
+      arrayAbout.push({
+        description: el.description,
+        name: el.name,
+        temperament: el.temperament,
+      })
+    );
   })
-  .catch(onError);
+  .catch(err => {
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+  });
 
 function onSelectClick(evt) {
   const breedId = evt.target.value;
   // console.log(breedId);
 
+  select.classList.add('hidden');
+  loader.classList.remove('hidden');
+  catInfo.classList.add('hidden');
+
   fetchCatByBreed(breedId)
     .then(r => {
-      console.log(r);
+      select.classList.remove('hidden');
+      //     console.log(r);
+      // console.log(arrayAbout);
 
-      const catData = r.data;
+      const catData = r.data[0];
       console.log(catData);
-
-      return `<img src="${catData.url}" alt="${catData.name} photo" width="410">
-      <h2>Breed: ${catData.name}</h2>
-      <p>Description: ${catData.description}</p>
-      <p>Tempetament: ${catData.tempetament}</p>`;
+      // console.log(arrayAbout);
+      return `<img src="${r.data.url}" alt="" width="410">`;
+      // <h2>Breed: ${arrayAbout.name}</h2>
+      // <p>Description: ${arrayAbout.description}</p>
+      // <p>Tempetament: ${arrayAbout.tempetament}</p>`;
     })
     .then(r => {
-      return (catInfo.innerHTML = r);
+      loader.classList.add('hidden');
+      catInfo.classList.remove('hidden');
+      return (catInfo.insertAdjacentElement = r);
+    })
+    .catch(err => {
+      select.classList.remove('hidden');
+      loader.classList.add('hidden');
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
     });
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * *
-function onError(err) {
-  Notiflix.Notify.failure(
-    'Oops! Something went wrong! Try reloading the page!'
-  );
+
+function createMarkup(url, { name, description, tempetament }) {
+  return `<img src="${url}" alt="" width="410">
+  <h2>Breed: ${name}</h2>
+  <p>Description: ${description}</p>
+  <p>Tempetament: ${tempetament}</p>`;
 }
